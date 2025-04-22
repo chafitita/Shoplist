46cd075d1c0d429155ba960e5fc37dfc57a72a0d
Levrino Mateo <mlevrino@facultad.sanfrancisco.utn.edu.ar>
2025-04-22 02:02:40 -0300

    validaciones update

--- src/components/ShopAdd.jsx
+++ src/components/ShopAdd.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useForm } from '../hooks/useForm'
 import agregarIcon from '../assets/agregar-btn.jpg'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../css/shopadd.css'
 
 export const ShopAdd = ({handleNewItem}) => {
 
@@ -11,7 +14,37 @@ export const ShopAdd = ({handleNewItem}) => {
   const onFormSubmit = e => {
     e.preventDefault()
 
-    if(description.length <= 1 || quantity <= 0) return 
+    if(description.length <= 1){
+      toast.error("Escribe algo!!",{
+        className: 'error-text',
+        hideProgressBar: true,
+        autoClose: 1500})
+      return;
+    }
+
+    if(quantity == 0) {
+      toast.warning("La cantidad no puede ser nula.",{
+        className: 'warning-nulidad',
+        hideProgressBar: true,
+        autoClose: 1250});
+      return;
+    }
+
+    if(quantity < 0){
+      toast.warning("La cantidad no puede ser negativa.",{
+        className: 'warning-menor',
+        hideProgressBar: true,
+        autoClose: 1250})
+      return;
+    }
+
+    if(quantity >= 1000) {
+      toast.warning("La cantidad excede el límite de 1000.", {
+        className: 'error-cantidad',
+        hideProgressBar: true,
+        autoClose: 1250});
+      return;
+    }
 
     let newItem = {
       id: new Date().getTime(),
@@ -25,27 +58,31 @@ export const ShopAdd = ({handleNewItem}) => {
   }
 
   return (
-    <form onSubmit={onFormSubmit}>
-        <input 
-        type="text" 
-        name="description" 
-        className='input-add' 
-        value={description}
-        onChange={onInputChange}
-        placeholder='¿Que vas a comprar hoy?'
-        />
-        <input
-        type="number"
-        name="quantity"
-        className="input-add"
-        value={quantity}
-        onChange={onInputChange}
-        placeholder="Cantidad"
-        />
-        <button className="btn-add" type='submit'>
-            <img src={agregarIcon} alt="agregar" />
-        </button>
-    </form>
+    <>
+      <ToastContainer />
+      <form onSubmit={onFormSubmit}>
+          <input 
+          type="text" 
+          name="description" 
+          className='input-add' 
+          value={description}
+          onChange={onInputChange}
+          placeholder='¿Que vas a comprar hoy?'
+          />
+          <input
+          type="number"
+          name="quantity"
+          className="input-add"
+          value={quantity}
+          onChange={onInputChange}
+          placeholder="Cantidad"
+          />
+          <button className="btn-add" type='submit'>
+              <img src={agregarIcon} alt="agregar" />
+          </button>
+      </form>
+    </>
+    
   )
 }
 export default ShopAdd
